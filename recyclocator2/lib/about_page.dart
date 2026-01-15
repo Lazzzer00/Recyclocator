38771c31a408651c97bc196ecd39325cdef56e52
Lazar Knezevic <141447582+Lazzzer00@users.noreply.github.com>
2026-01-15 12:32:54 +0100

    Add files via upload

---
+++ recyclocator2/lib/about_page.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';		//flutter ima ugradjen library za make
+import 'package:http/http.dart' as http;
+
+class AboutPage extends StatefulWidget {
+  const AboutPage({super.key});
+  @override
+  _AboutPageState createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  Set<Marker> _markers = {};
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMarkers();
+  } 
+
+  Future<void> _fetchMarkers() async {
+    final response = await http.get(
+      Uri.parse('https://lokacije.gimnazija.net/api/locations'),    //ovo je link sa kojeg dobijamo pozicije pinova, sa servera
+    );
+
+    if (response.statusCode == 200) {
+      final List data = json.decode(response.body);
+
+      final markers = data.map((item) {
+        return Marker(
+          markerId: MarkerId('${item['lat']}_${item['lng']}'),
+          position: LatLng(item['lat'], item['lng']),
+        );
+      }).toSet();
+
+      setState(() {
+        _markers = markers;
+        _loading = false;
+      });
+    } else {
+      setState(() {
+        _loading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_loading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return GoogleMap(
+      initialCameraPosition: const CameraPosition(
+        target: LatLng(44.733, 18.098), // Pocetna pozicija mape, ovo su otprilike koordinate doboja
+        zoom: 7,
+      ),
+      markers: _markers,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: true,
+    );
+  }
+}
